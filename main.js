8ef5b5ecf72108006f2d9da9a7579fe39b59805c
luis david <luisdavid_204@hotmail.com>
2024-05-08 20:39:52 -0300

    inserindo os arquivos principais

---
+++ main.js
@@ -0,0 +1,63 @@
+/* function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+
+}
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+
+}
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+
+}
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+
+}
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+
+}
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+
+}
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+
+}
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tic').play();
+
+}
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomClap;
+document.querySelector('.tecla_pom').onclick = tocaSomTim;
+document.querySelector('.tecla_pom').onclick = tocaSomPuff;
+document.querySelector('.tecla_pom').onclick = tocaSomSplash;
+document.querySelector('.tecla_pom').onclick = tocaSomToim;
+document.querySelector('.tecla_pom').onclick = tocaSomPsh;
+document.querySelector('.tecla_pom').onclick = tocaSomTim;
+document.querySelector('.tecla_pom').onclick = tocaSomTom;
+
+ */
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas =  document.querySelectorAll('.tecla');
+
+listaDeTeclas [0].onclick = tocaSomPom;
+listaDeTeclas [0].onclick = tocaSomClap;
+listaDeTeclas [0].onclick = tocaSomTim;
+listaDeTeclas [0].onclick = tocaSomPuff;
+listaDeTeclas [0].onclick = tocaSomSplash;
+listaDeTeclas [0].onclick = tocaSomPsh;
+listaDeTeclas [0].onclick = tocaSomTim;
+listaDeTeclas [0].onclick = tocaSomTom;
+
